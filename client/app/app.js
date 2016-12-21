e7ab2db8333310b40f2873a92e81fef61786b790
Jason <jmaier@berkeley.edu>
2016-12-21 15:23:53 -0800

    homepage work

--- client/app/app.js
+++ client/app/app.js
@@ -3,18 +3,14 @@ angular.module('blog', ['ngRoute'])
   $routeProvider
     .when('/', {
       templateUrl: 'app/landing.html',
-      // controller: 'landingCtrl'
     })
     .when('/linkedlists', {
       templateUrl: 'app/linkedlists.html',
-      // controller: 'nomadCtrl'
     })
     .when('/stacksandqueues', {
       templateUrl: 'app/stacksandqueues.html',
-      // controller: 'explorerCtrl'
     })
     .when('/about', {
       templateUrl: 'app/about.html',
-      // controller: 'explorerCtrl'
     })
 })
